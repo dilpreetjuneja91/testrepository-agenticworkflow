cf7a33625037972754e6497ef6fea13fe129ec52
dilpreetjuneja91 <65270973+dilpreetjuneja91@users.noreply.github.com>
2026-04-14 12:31:21 -0400

    first commit
    first commit to test agenticworkflow

---
+++ agenticworkflow/src/main/java/com/dj/agenticworkflow/AgenticworkflowApplication.java
@@ -0,0 +1,13 @@
+package com.dj.agenticworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticworkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticworkflowApplication.class, args);
+	}
+
+}
